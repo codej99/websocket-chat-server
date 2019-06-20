a76b80a6ba3b7d2cdfe9aa441a50eb2f5f43f808
kimyonghwa <codej99@nhnent.com>
2019-06-20 11:53:35 +0900

    Develop ChatRoom
    - Update Comment

--- src/main/java/com/websocket/chat/config/handler/StompHandler.java
+++ src/main/java/com/websocket/chat/config/handler/StompHandler.java
@@ -52,7 +52,7 @@ public class StompHandler implements ChannelInterceptor {
             String roomId = chatRoomRepository.getUserEnterRoomId(sessionId);
             // 채팅방의 인원수를 -1한다.
             chatRoomRepository.minusUserCount(roomId);
-            // 클라이언트 장 메시지를 채팅방에 발송한다.(redis publish)
+            // 클라이언트 퇴장 메시지를 채팅방에 발송한다.(redis publish)
             String name = Optional.ofNullable((Principal) message.getHeaders().get("simpUser")).map(Principal::getName).orElse("UnknownUser");
             chatService.sendChatMessage(ChatMessage.builder().type(ChatMessage.MessageType.QUIT).roomId(roomId).sender(name).build());
             // 퇴장한 클라이언트의 roomId 맵핑 정보를 삭제한다.
